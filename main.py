6c24c3a6bf414672a826758d0a1e7228c7b692ec
patasola <94874250+patasola@users.noreply.github.com>
2025-06-26 16:10:19 -0500

    Upload main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# main.py - Archivo Principal de la Aplicación
+"""
+Búsqueda Rápida de Carpetas v1.4
+Archivo principal de la aplicación
+"""
+
+import tkinter as tk
+from src.app import BusquedaCarpetaApp
+
+def main():
+    """Función principal para iniciar la aplicación"""
+    root = tk.Tk()
+    app = BusquedaCarpetaApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
